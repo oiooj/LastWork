58fe1aaf893b1bda694959ce97775aadc5ffd62e
baobao <nototon@126.com>
2013-01-23 19:55:08 +0800

    init repo

---
+++ include/functions.php
@@ -0,0 +1,132 @@
+<?php
+	
+	require_once("config.php");
+	require_once( "memcached-client.php");
+	
+	
+	///////////////////////////////////MongoDB//////////////////////////////////////////
+	
+	//Connect to MongoDB
+	function Get_Mongodb($server="mongodb://192.168.139.100:27017"){
+		return new Mongo($server); 
+	}
+	//Close MongoDB
+	function Close_Mongodb($mogo){
+		$mogo->close(); 
+	}	
+	
+	//MongoDB Writter
+	function Mongodb_Writter($collection_name,$data){
+		global $SYS_CONFIG;
+		$mogo = Get_Mongodb($SYS_CONFIG["DB"]["MASTER"]);
+		$db = $mogo -> $SYS_CONFIG["DB"]["DATABASE"];
+		$collection = $db ->$collection_name;
+		$collection->insert($data);
+		Close_Mongodb($mogo);
+	}
+	
+	//MongDB Reader
+	//return N records (todo)
+	function Mongodb_Reader($collection_name,$condition,$type=0){
+		global $SYS_CONFIG;
+		$mogo = Get_Mongodb($SYS_CONFIG["DB"]["MASTER"]);
+		$db = $mogo -> $SYS_CONFIG["DB"]["DATABASE"];
+		$collection = $db ->$collection_name;
+		if($type == 1){
+			$key = Create_Key($collection_name.$condition);
+			if($data = Memcached_Getter($key)){
+				return $data;
+			}
+			$data = $collection->findOne($condition);
+			Memcached_Setter($key,$data);
+		}
+		else if ($type == 0){
+			$data = $collection->find($condition);
+		}
+		else{
+
+		}
+		Close_Mongodb($mogo);
+		return $data;
+	}
+	
+	//MongoDB Remover
+	function Mongodb_Remover($collection_name,$condition,$type=0){
+		global $SYS_CONFIG;
+		$mogo = Get_Mongodb($SYS_CONFIG["DB"]["MASTER"]);
+		$db = $mogo -> $SYS_CONFIG["DB"]["DATABASE"];
+		$collection = $db ->$collection_name;
+		if($type == 1){
+			$res = $collection->remove($condition,array("justOne" => true));
+		}
+		else{
+			$res = $collection->remove($condition);
+		}
+		Close_Mongodb($mogo);
+		$key = Create_Key($collection_name.$condition);
+		Memcached_Remover($key);
+		return $res;
+	}
+
+	//MongoDB Updater
+	function Mongodb_Updater($collection_name,$condition,$data){
+		global $SYS_CONFIG;
+		$mogo = Get_Mongodb($SYS_CONFIG["DB"]["MASTER"]);
+		$db = $mogo -> $SYS_CONFIG["DB"]["DATABASE"];
+		$collection = $db ->$collection_name;
+		$collection->update($condition,array('$set' => $data));
+		Close_Mongodb($mogo);
+		$key = Create_Key($collection_name.$condition);
+		Memcached_Remover($key);
+	}
+	
+	
+	
+	
+	///////////////////////////////////Memcached/////////////////////////////////////////
+	
+	//Connect to Memcached Servers
+	function Get_Memcached(){
+		global $SYS_CONFIG;
+		return new memcached($SYS_CONFIG["MEMCACHE_SERVERS"]);
+	}
+
+	//Close Memcached
+	function Close_Memcached($m){
+		$m->close();
+	}
+	
+	//Memcached Setter
+	function Memcached_Setter($key,$value){
+		$m = Get_Memcached();
+		$m -> set ($key,$value);
+		Close_Memcached($m);
+	}
+	
+	//Memcached Getter
+	function Memcached_Getter($key){
+		$m = Get_Memcached();
+		$res = $m -> get ($key);
+		Close_Memcached($m);
+		if($res == false || $res == 16){
+			return false;
+		}
+		return $res;
+	}
+	
+	//Memcached Rrmover
+	function Memcached_Remover($key){
+		$m = Get_Memcached();
+		$m -> delete($key);
+		Close_Memcached($m);
+	}
+	
+	///////////////////////////////////Utils/////////////////////////////////////////	
+	
+	function Create_Key($str){
+		return md5(trim($str));
+	}
+	
+	
+	
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	//Just for test here~
+
+
+	require_once(dirname(__FILE__) .'/include/functions.php');
+
+	// $data = array("name" => "last","email" => "baidu2@qq.com","age" => 23);
+	// Mongodb_Writter("foo",$data);
+
+	$res = Mongodb_Reader("foo",array("name" => "poji"),1);
+	echo $res['name'];
+	//Mongodb_Updater("foo",array("name" => "last"),array("name" => "poji"));
+	echo "<br/><br/><br/>";
+	var_dump( Memcached_Getter(Create_Key("foo".array("name" => "last"))));
+
+
+?>
