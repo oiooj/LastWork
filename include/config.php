e40296ea071f4c8e2014a7b998557e0d5da3be3e
oiooj <oiooj@qq.com>
2013-04-01 08:59:06 +0800

    Update config.php
    used host to connect

--- include/config.php
+++ include/config.php
@@ -2,11 +2,11 @@
 error_reporting(E_ALL);
 
 $SYS_CONFIG["DB"]["TYPE"]       = "mongodb";		  
-$SYS_CONFIG["DB"]["MASTER"]     = "mongodb://192.168.139.100:27017";
-$SYS_CONFIG["DB"]["SLAVE"]      = "mongodb://192.168.139.101:27017";
-#$SYS_CONFIG["DB"]["MASTER"]     = "mongodb://oiooj:123@ds043977.mongolab.com:43977/web";  
-#$SYS_CONFIG["DB"]["SLAVE"]      = "mongodb://oiooj:123@ds043977.mongolab.com:43977/web"; 
-#$SYS_CONFIG["DB"]["DATABASE"]   = "web";
+$SYS_CONFIG["DB"]["MASTER"]     = "mongodb://db@100:27017";
+$SYS_CONFIG["DB"]["SLAVE"]      = "mongodb://db@101:27017";
+#$SYS_CONFIG["DB"]["MASTER"]    = "mongodb://admin:123@ds043977.mongolab.com:43977/web";  
+#$SYS_CONFIG["DB"]["SLAVE"]     = "mongodb://admin:123@ds043977.mongolab.com:43977/web"; 
+#$SYS_CONFIG["DB"]["DATABASE"]  = "web_";
 $SYS_CONFIG["DB"]["DATABASE"]   = "Web_Design_Todo";
 $SYS_CONFIG["DB"]["CHARSET"]    = "utf8";
 
@@ -17,8 +17,8 @@ $SYS_CONFIG["OS"] 				= "CentOS";
 
 $SYS_CONFIG["TIME"]["START"]       = Microtime_Float();
 $SYS_CONFIG["USE_MEMCACHE"]        = false;
-$SYS_CONFIG["ACCESSTOKEN_SERVERS"] = array(  'servers' => array('192.168.139.103:11211'),'debug'   => false,'compress_threshold' => 10240,'persistant' => true);
-$SYS_CONFIG["MEMCACHE_SERVERS"]    = array(  'servers' => array('192.168.139.102:11211'),'debug'   => false,'compress_threshold' => 10240,'persistant' => true);
+$SYS_CONFIG["ACCESSTOKEN_SERVERS"] = array(  'servers' => array('cache@103:11211'),'debug'   => false,'compress_threshold' => 10240,'persistant' => true);
+$SYS_CONFIG["MEMCACHE_SERVERS"]    = array(  'servers' => array('cache@102:11211'),'debug'   => false,'compress_threshold' => 10240,'persistant' => true);
 $SYS_CONFIG["ACCESS_TOKEN_TIME"]   = 600;
 
 $SYS_CONFIG["EMAIL"]["HOST"]       = "smtp.126.com";
